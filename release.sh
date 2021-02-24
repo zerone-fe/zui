026779e5928935b68bd61e56f531aae3e21a547b
haobing <liamwang@aliyun.com>
2021-02-24 15:19:35 +0800

    更新发布脚本

--- release.sh
+++ release.sh
@@ -11,5 +11,8 @@ npm version patch
 
 npm publish
 
-# 发布完成后同步到淘宝镜像
+# 发布完成后同步到淘宝镜像，淘宝镜像源10分钟才会同步一次
+# 通过命令行同步模块
 cnpm sync @zerone-fe/zui
+# 通过 web 方式来同步模块
+open https://npm.taobao.org/sync/@zerone-fe/zui
